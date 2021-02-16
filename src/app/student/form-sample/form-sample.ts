d7ee4840c843d4b641bc567ae9168ddc9284b875
WANZILA RONNY <wanzilro@gmail.com>
2021-02-16 10:10:03 +0300

    feat: redesigne of student Application

---
+++ src/app/student/form-sample/form-sample.ts
@@ -0,0 +1,8 @@
+export class FormSample{
+  constructor(
+    public fname= "",
+    public lname="",
+    public email="",
+    public phone=""
+  ){ }
+}
--- src/app/student/student-application/student-application.component.ts
+++ src/app/student/student-application/student-application.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-// import { FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder,Validators } from '@angular/forms';
+import { StudentDetails } from '../student';
 
 @Component({
   selector: 'app-student-application',
@@ -7,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-application.component.css']
 })
 export class StudentApplicationComponent implements OnInit {
+  //reference to FormGroup Model
+  //studentForm: FormGroup;
+  generalForm: FormGroup;
 
-  constructor() { }
+  //data modelt for managing student data
+  students: StudentDetails = new StudentDetails();
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+
+    // nb add religion 
+    this.generalForm = this.fb.group({
+    studentId:['',[Validators.required, Validators.minLength(2)]] ,
+    fname:['',[Validators.required, Validators.minLength(2)]] ,
+    mName:['',[Validators.required, Validators.minLength(2)]] ,
+    lname:['',[Validators.required, Validators.minLength(2)]] ,
+    title:['',[Validators.required, Validators.minLength(2)]] ,
+    birthDate:['',[Validators.required, Validators.minLength(2)]] ,
+    gender:['',[Validators.required, Validators.minLength(2)]] ,
+    maritalStatus:['',[Validators.required, Validators.minLength(2)]] ,
+     
+    });
+
+
+
   }
   // fnameControl = new FormControl('');
 
